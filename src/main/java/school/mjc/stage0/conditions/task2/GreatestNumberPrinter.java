4eeae464bb4ae93c281b7aefa3eda860cd0d22bd
khatia ormotsadze <khatiaormotsadze99@gmail.com>
2023-06-11 12:51:23 +0400

    git commit -m "Modify CoinFlip.java" -m "Modify GreatestNumberPrinter.java"

--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.conditions.task2;
 
 public class GreatestNumberPrinter {
     public void printGreatest(int first, int second) {
+
+        if(first > second){
+            System.out.println("The greatest number is:" + first);
+        } else if (second > first) {
+            System.out.println("The greatest number is:" + second);
+        }  else if (second == first) {
+            System.out.println(first + "" + "is equal to" + "" + second);
+        }
     }
 }
